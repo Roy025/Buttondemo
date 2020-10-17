b6877ab9f004485b76f815c054be132181437f00
roy <53114983+Roy025@users.noreply.github.com>
2020-10-18 02:51:32 +0600

    Added Toast

--- app/src/main/java/com/example/buttondemo/MainActivity.java
+++ app/src/main/java/com/example/buttondemo/MainActivity.java
@@ -3,35 +3,41 @@ package com.example.buttondemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import static com.example.buttondemo.R.id.center;
+import static com.example.buttondemo.R.id.login;
 
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+
+    private Button login,logout;
     private TextView textView;
     int c = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button logout = (Button) findViewById(R.id.logout);
-        Button login = (Button)findViewById(R.id.login);
+        logout = (Button) findViewById(R.id.logout);
+         login = (Button)findViewById(R.id.login);
         textView = (TextView)findViewById(R.id.textView);
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                c++;
-                textView.setText("Logout button is clicked " + c + " times. ");
-            }
-        });
-        c = 0;
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                c++;
-                textView.setText("Login button is clicked " + c + " times. ");
-            }
-        });
+        login.setOnClickListener(this);
+        logout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast toast;
+        if(v.getId()==R.id.login) {
+             toast = Toast.makeText(MainActivity.this,"Login Button is clicked",Toast.LENGTH_SHORT);
+        } else {
+             toast = Toast.makeText(MainActivity.this,"Logiout Button is clicked",Toast.LENGTH_SHORT);
+        }
+             toast.setGravity(Gravity.CENTER ,0,0);
+             toast.show();
+
     }
 }
