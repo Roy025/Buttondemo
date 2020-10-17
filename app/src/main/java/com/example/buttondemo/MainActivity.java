c1345181217390fd94c6855b7f6b76605aee2cd6
roy <53114983+Roy025@users.noreply.github.com>
2020-10-18 02:23:59 +0600

    Added Listener

--- app/src/main/java/com/example/buttondemo/MainActivity.java
+++ app/src/main/java/com/example/buttondemo/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.buttondemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
+    int c = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button logout = (Button) findViewById(R.id.logout);
+        Button login = (Button)findViewById(R.id.login);
+        textView = (TextView)findViewById(R.id.textView);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                c++;
+                textView.setText("Logout button is clicked " + c + " times. ");
+            }
+        });
+        c = 0;
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                c++;
+                textView.setText("Login button is clicked " + c + " times. ");
+            }
+        });
     }
 }
